41488ed2c46be494411487ab79d444364589e475
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2025-03-27 13:08:23 +0100

    minor

--- pagerduty-xbar/xbar_pagerduty_plugin.sh
+++ pagerduty-xbar/xbar_pagerduty_plugin.sh
@@ -3,7 +3,8 @@
 # <xbar.title>PagerDuty status</xbar.title>
 # <xbar.version>v0.0.1</xbar.version>
 # <xbar.author>Jakub Mikians</xbar.author>
-# <xbar.author.github>jakub-m-m</xbar.author.github>
+# <xbar.author.github>jakub-m</xbar.author.github>
 # <xbar.desc>Show PagerDuty status.</xbar.desc>
 
+# !!! Set this to the full path of the script
 exec ./xbar_pagerduty.sh

0eac1f2a4b7fd39f312516dd523c71918833ba32
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2025-03-27 13:00:23 +0100

    run relase target

--- pagerduty-xbar/xbar_pagerduty.sh
+++ pagerduty-xbar/xbar_pagerduty.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -eu
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
 config_file=""
 if [[ -e "$PWD/.pg_xbar.conf" ]]; then
-  config_file=""$PWD/.pg_xbar.conf""
+  config_file="$PWD/.pg_xbar.conf"
 fi
 
 if [[ -f "$HOME/.config/pg_xbar.conf" ]]; then
@@ -15,9 +17,10 @@ if [[ -z "${config_file}" ]]; then
   exit 1
 fi
 
-2>echo "config_file: ${config_file}"
+>&2 echo "config_file: ${config_file}"
 
 set -a
 source "${config_file}"
 set +a
-cargo run | tee response.json
+# cargo run
+exec "${SCRIPT_DIR}/target/release/pagerduty-xbar"

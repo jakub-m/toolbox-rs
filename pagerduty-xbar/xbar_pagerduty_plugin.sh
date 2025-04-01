47fcd343dbf306b7b4a6e4f5d02bc91391aa1d0e
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2025-04-01 21:52:42 +0200

    Minor update in README

--- pagerduty-xbar/xbar_pagerduty_plugin.sh
+++ pagerduty-xbar/xbar_pagerduty_plugin.sh
@@ -2,7 +2,6 @@
 
 # <xbar.title>PagerDuty status</xbar.title>
 # <xbar.version>v0.1.0</xbar.version>
-# <xbar.author>Jakub Mikians</xbar.author>
 # <xbar.author.github>jakub-m</xbar.author.github>
 # <xbar.desc>Show PagerDuty status.</xbar.desc>
 

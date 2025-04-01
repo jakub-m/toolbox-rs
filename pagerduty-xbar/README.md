7a41fdeb05cfc7520d7276db930578b9da171fdc
LiquidLemon <9415789+LiquidLemon@users.noreply.github.com>
2025-04-01 21:46:42 +0200

    Show upcoming shifts (and minor enhancements)  (#1)
    * .gitignore: add .vscode/
    
    * deps: enable `serde` feature in `chrono`
    
    * plugin.sh: make file linkable
    
    * xbar_pagerduty.sh: show error when file is not found
    
    * extend output with information about upcoming shifts
    
    * README.md: add installation instruction

--- pagerduty-xbar/README.md
+++ pagerduty-xbar/README.md
@@ -1,11 +1,13 @@
 # pagerduty-xbar
 
-Show Pager duty status in xbar.
+Show Pager duty status in [xbar][xbar].
+
+[xbar]: https://github.com/matryer/xbar
 
 Requires the following environment variables to work (in `~/.config/pg_xbar.conf`)
 - `PG_SCHEDULE_ID` - ID of the schedule, can be take from PagerDuty URL.
 - `PG_AUTH_TOKEN` - Authentication token, generate it in PagerDuty settings.
-- `PG_USER` - The full name of the user that is looked up in the PagerDuty response.
+- `PG_USER_ID` - your user ID, available in your profile URL.
 - `PG_ICONS` - Optional set of there icons: on-duty, tomorrow-on-duty, not-on-duty.
 
 ## Running
@@ -17,3 +19,11 @@ cargo build --relase
 
 For **development** (when run from within the git repository) the script uses
 `cargo run` instead of release target.
+
+## Installation
+
+```sh
+$ git clone git@github.com:jakub-m/toolbox-rs.git
+$ cargo build --release
+$ ln -s $(realpath xbar_pagerduty_plugin.sh) "~/Library/Application Support/xbar/plugins/pagerduty.1h.sh"
+```
--- pagerduty-xbar/src/main.rs
+++ pagerduty-xbar/src/main.rs
@@ -1,18 +1,9 @@
-use chrono::{DateTime, Duration, SecondsFormat, Utc};
+use chrono::{DateTime, Duration, Local, SecondsFormat, TimeDelta, Utc};
 use reqwest::Client;
-use reqwest::header::{AUTHORIZATION, CONTENT_TYPE, HeaderMap, HeaderValue};
-use serde::Deserialize;
 use std::env;
+use types::{Oncall, Oncalls};
 
-#[derive(Debug, Deserialize)]
-struct UsersWrapper {
-    users: Vec<User>,
-}
-
-#[derive(Debug, Deserialize)]
-struct User {
-    name: String,
-}
+mod types;
 
 const GREEN_CIRCLE: char = char::from_u32(0x1F7E2).unwrap();
 const YELLOW_CIRCLE: char = char::from_u32(0x1F7E1).unwrap();
@@ -22,26 +13,21 @@ const CENTER_X: char = char::from_u32(0x00D7).unwrap();
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let pg_schedule_id = env_var("PG_SCHEDULE_ID")?;
     let pg_auth_token = env_var("PG_AUTH_TOKEN")?;
-    let pg_user = env_var("PG_USER")?;
+    let pg_user_id = env_var("PG_USER_ID")?;
     let pg_icons = env_var("PG_ICONS").unwrap_or("".to_owned());
     let now = Utc::now();
-    let is_oncall_now = check_if_user_on_call(
+
+    let oncalls = get_oncalls(
         now,
-        now + Duration::minutes(1),
+        now + Duration::days(14),
         &pg_auth_token,
         &pg_schedule_id,
-        &pg_user,
+        &pg_user_id,
     )
     .await?;
 
-    let is_oncall_next_24h = check_if_user_on_call(
-        now,
-        now + Duration::days(1),
-        &pg_auth_token,
-        &pg_schedule_id,
-        &pg_user,
-    )
-    .await?;
+    let is_oncall_now = is_oncall(now, &oncalls);
+    let is_oncall_next_24h = is_oncall(now + Duration::days(1), &oncalls);
 
     let (icon_on_duty, icon_incoming_on_duty, icon_not_on_duty) = get_icons(&pg_icons);
 
@@ -53,36 +39,77 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("{icon_not_on_duty}");
     }
 
+    println!("---");
+    println!("Upcoming:");
+
+    for oncall in &oncalls {
+        let delta = oncall.start - now;
+
+        let time_remaining = if delta <= TimeDelta::zero() {
+            "now".into()
+        } else {
+            let days = delta.num_days();
+            if days > 0 {
+                format!("{}d", days)
+            } else {
+                "<1d".into()
+            }
+        };
+
+        println!(
+            "{} - ({}) {} - {}",
+            oncall.schedule.summary,
+            time_remaining,
+            format_readable(oncall.start),
+            format_readable(oncall.end)
+        );
+    }
+
     Ok(())
 }
 
-/// https://developer.pagerduty.com/api-reference/e1ad560792567-list-users-on-call
-async fn check_if_user_on_call(
-    shift_from: DateTime<Utc>,
-    shift_to: DateTime<Utc>,
+fn is_oncall(time: DateTime<Utc>, oncalls: &[Oncall]) -> bool {
+    oncalls.iter().any(|o| time >= o.start && time < o.end)
+}
+
+fn format_readable(dt: DateTime<Utc>) -> String {
+    let local: DateTime<Local> = dt.into();
+    local.format("%a %d-%b-%Y %H:%M").to_string()
+}
+
+fn format_iso(dt: DateTime<Utc>) -> String {
+    dt.to_rfc3339_opts(SecondsFormat::Secs, true)
+}
+
+/// https://developer.pagerduty.com/api-reference/3a6b910f11050-list-all-of-the-on-calls
+async fn get_oncalls(
+    from: DateTime<Utc>,
+    to: DateTime<Utc>,
     pg_auth_token: &str,
     pg_schedule_id: &str,
-    pg_user: &str,
-) -> Result<bool, Box<dyn std::error::Error>> {
-    let since_iso = shift_from.to_rfc3339_opts(SecondsFormat::Secs, true);
-    let until_iso = shift_to.to_rfc3339_opts(SecondsFormat::Secs, true);
+    pg_user_id: &str,
+) -> Result<Vec<Oncall>, Box<dyn std::error::Error>> {
     let client = Client::new();
-    let mut headers = HeaderMap::new();
-    headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
-    let authorization = format!("Token token={pg_auth_token}");
-    headers.insert(AUTHORIZATION, HeaderValue::from_str(&authorization)?);
-    let url = format!(
-        "https://api.pagerduty.com/schedules/{pg_schedule_id}/users?since={since_iso}&until={until_iso}"
-    );
-    eprintln!("{url}");
-    let res = client.get(url).headers(headers).send().await?;
+    let res = client
+        .get("https://api.pagerduty.com/oncalls")
+        .query(&[
+            ("schedule_ids[]", pg_schedule_id),
+            ("since", &format_iso(from)),
+            ("until", &format_iso(to)),
+        ])
+        .header("Authorization", format!("Token token={pg_auth_token}"))
+        .send()
+        .await?;
 
-    let response_body = res.text().await?;
-    eprintln!("{response_body}");
+    let oncalls: Oncalls = res.json().await?;
 
-    let parsed: UsersWrapper = serde_json::from_str(&response_body).unwrap();
-    let is_oncall = parsed.users.iter().any(|user| user.name == pg_user);
-    Ok(is_oncall)
+    let user_oncalls = oncalls
+        .oncalls
+        .into_iter()
+        .filter(|o| o.user.id == pg_user_id)
+        .collect();
+
+    Ok(user_oncalls)
 }
 
 fn env_var(name: &str) -> Result<String, String> {
@@ -92,7 +119,7 @@ fn env_var(name: &str) -> Result<String, String> {
 fn get_icons(icons: &str) -> (char, char, char) {
     let icons: Vec<char> = icons.chars().collect();
     (
-        *icons.get(0).unwrap_or(&GREEN_CIRCLE),
+        *icons.first().unwrap_or(&GREEN_CIRCLE),
         *icons.get(1).unwrap_or(&YELLOW_CIRCLE),
         *icons.get(2).unwrap_or(&CENTER_X),
     )

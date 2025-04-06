a467d52d3a65134fe84fe45af9951c05dfc0349c
Jakub Mikians <2897102+jakub-m@users.noreply.github.com>
2025-04-06 22:08:15 +0200

    Move to other repo

--- pagerduty-xbar/xbar_pagerduty_plugin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# <xbar.title>PagerDuty status</xbar.title>
-# <xbar.version>v0.1.0</xbar.version>
-# <xbar.author.github>jakub-m</xbar.author.github>
-# <xbar.desc>Show PagerDuty status.</xbar.desc>
-
-SCRIPT_DIR=$(dirname -- $(realpath "${BASH_SOURCE[0]}"))
-exec "${SCRIPT_DIR}/xbar_pagerduty.sh"
